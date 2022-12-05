c685c66fa54f7225ac620f98f760be34828db20e
radhika <radhikamedukonduru212@gmail.com>
2022-12-05 14:38:53 -0500

    First commit

---
+++ src/main/java/com/spring/restservices/SpringBootBuildBlocksApplication.java
@@ -0,0 +1,13 @@
+package com.spring.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuildBlocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBuildBlocksApplication.class, args);
+	}
+
+}
